c55d67b142efa0867214b87bfe179b99afe314b1
Hossein Sarshar <hossein.sarshar@gmail.com>
2024-04-07 05:23:51 +0000

    a

--- session-02/time_jit.py
+++ session-02/time_jit.py
@@ -2,12 +2,15 @@
 
 import datetime
 import jax
+from jax import jit
+from timing_util import simple_timeit
 
 MATRIX_DIM = 32768
 STEPS = 10
 
 A = jax.numpy.ones((MATRIX_DIM, MATRIX_DIM))
 B = jax.numpy.ones((MATRIX_DIM, MATRIX_DIM))
+C = jax.numpy.ones((MATRIX_DIM, MATRIX_DIM))
 
 num_bytes = A.size * 4 # 4 as it's fp32
 print('A.size -> ', A.size) # 1073741824
@@ -15,14 +18,14 @@ total_num_bytes_crossing_to_hbm = num_bytes * 3 # A, B, and C
 
 total_num_flops = MATRIX_DIM * MATRIX_DIM
 
-jax.profiler.start_trace('gs://maxtext-logs-dogfood-proj/jax-llm/dev-env/raw') ## -> tensorboard --logdir=gs://maxtext-logs-dogfood-proj/jax-llm/dev-env/raw
-starttime = datetime.datetime.now()
 
-for i in range(STEPS):
-    C = A + B
+def f(A, B):
+    return A + B
 
-endtime = datetime.datetime.now()
-jax.profiler.stop_trace()
-average_time = (endtime - starttime).total_seconds() / STEPS
+jit_f = jit(f)
+
+average_time = simple_timeit(jit_f, A, B, task="mytask")/1000
 
 print(f"{average_time}, tera flops per sec {total_num_flops/average_time / 10**12}, giga bytes per second {total_num_bytes_crossing_to_hbm/average_time / 10**9}")
+
+## 
